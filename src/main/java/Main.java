f4887bdbd4418b872408d2e66cbe75d2e7d5a66a
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-03-01 11:08:37 -0500

    I did both extra credit :)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
@@ -8,6 +9,8 @@ public class Main {
         int number = getInputNumber();
         
         int sum = sumOfNumbers(number);
+
+        timeTester(number);
         
         System.out.println(sum);
 
@@ -15,12 +18,17 @@ public class Main {
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int sum = 0;
+        for(int i = 0; i < n+1; i++){
+            sum+= i;
+        }
+
+        return sum;
     }
     
     
-    int getInputNumber() {
+    static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -35,9 +43,35 @@ public class Main {
     
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) {
+        return (n*(n+1))/2;
     }
+
+    static void timeTester(int number){
+        long start = 0;
+        long end = 0;
+        long loop = 0;
+        long gauss = 0;
+        start = System.currentTimeMillis();
+        sumOfNumbers(number);
+        end = System.currentTimeMillis();
+        loop = end-start;
+        start = System.currentTimeMillis();
+        gaussianSumOfNumbers(number);
+        end = System.currentTimeMillis();
+        gauss = end-start;
+        if(gauss<loop){
+            System.out.println("gaussian is faster");
+        }
+        else if(loop<gauss){
+            System.out.println("looping is faster");
+        }
+        else{
+            System.out.println("They match??");
+        }
+    }
+
+    //gaussian is faster, low values they are equal, i initially used a different time function.
    
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
